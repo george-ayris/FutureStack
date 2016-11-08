ac6a0a815304c303bd9c5459650cdd7bd1d09434
George Ayris <George.Ayris@huddle.local>
2016-11-08 18:56:33 +0000

    Todos methods

--- src/FutureStack/Controllers/TodosController.cs
+++ src/FutureStack/Controllers/TodosController.cs
@@ -7,35 +7,35 @@ using Microsoft.AspNetCore.Mvc;
 namespace FutureStack.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class TodosController : Controller
     {
-        // GET api/values
+        // GET api/todos
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/todos/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/todos
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/todos/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/todos/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
