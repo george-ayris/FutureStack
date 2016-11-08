9da4486645deebc88335e7feaae446a3558b4a25
George Ayris <George.Ayris@huddle.local>
2016-11-08 17:04:18 +0000

    Added api root

---
+++ src/FutureStack/Controllers/RootController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FutureStack.Controllers
+{
+    [Route("api")]
+    public class RootController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Alive and kicking";
+        }
+
+    }
+}
